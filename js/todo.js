d488345700a72e9d5314ac95f0b9ee7f210e0a57
jeffmeng <jkmexpress@yahoo.com>
2016-06-13 05:59:34 +0000

    bug fix

--- js/todo.js
+++ js/todo.js
@@ -1,9 +1,31 @@
-/* global $ */
-$( "a.hovertrigger" ).hover(
-  function() {
-    console.log(this);
-    $( this ).append( $( "<span><i class='fa fa-times' ></i></span>" ) );
-  }, function() {
-    $( this ).find( "span:last" ).remove();
-  }
+/* global $ firebase*/
+var database = firebase.database();
+
+$("a.hovertrigger").hover(
+	function() {
+		$(this).append($("<span><i class='fa fa-times' onclick='delete(" + $(this).attr("data-todo-id") + ")'></i></span>"));
+	},
+	function() {
+		$(this).find("span:last").remove();
+	}
 );
+
+function createTodo(name, uid) {
+	// TESTING ONLY!!!
+	uid = uid || "3409faojsniou309ofha393"
+  // A post entry.
+  var postData = {
+    shared:false,
+    name:name
+  };
+
+  // Get a key for a new Post.
+  var newPostKey = firebase.database().ref().child('posts').push().key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['/users/' + uid + '/lists/' + newPostKey] = postData;
+
+  return firebase.database().ref().update(updates);
+
+}
