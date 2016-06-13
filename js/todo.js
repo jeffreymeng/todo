ae6acf3f9f7bf566b06d138ec094607fc453a899
jeffmeng <jkmexpress@yahoo.com>
2016-06-13 05:46:34 +0000

    bug fix

--- js/todo.js
+++ js/todo.js
@@ -1,7 +1,8 @@
 /* global $ */
 $( "a.hovertrigger" ).hover(
   function() {
-    $( this ).append( $( "<span><i class='fa fa-times'></i></span>" ) );
+    console.log(this);
+    $( this ).append( $( "<span><i class='fa fa-times' ></i></span>" ) );
   }, function() {
     $( this ).find( "span:last" ).remove();
   }
