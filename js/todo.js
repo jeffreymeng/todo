62a1448ba557d80fbd870a9221f3d18ab66a8ebe
jeffmeng <jkmexpress@yahoo.com>
2016-06-17 17:43:09 +0000

    bug fix

--- js/todo.js
+++ js/todo.js
@@ -118,8 +118,7 @@ function addItem(id, name, description, deadline) {
 	if (description === null || description === undefined) {
 		description = "";
 	}
-	console.log((description === undefined && deadline === undefined))
-       if ((description === null && deadline === null) || (description === undefined && deadline === undefined)) {
+       if (description === '' && deadline === '') {
        	toappend = '' + 
 		'<hr><div id="item-' + id + '">' + 
 	        '<div class="checkbox checkbox-success">' + 
